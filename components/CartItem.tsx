aea88f0baa32f27c00d5b197d599d713a5f0f64b
Aerotheory <aerotheory@gmail.com>
2022-06-26 19:02:42 +1000

    added increase-decrease buttons for cart items update

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -18,35 +18,23 @@ export function CartItem({ item, cartId }: { item: CartItem; cartId: string }) {
       refetchQueries: [GetCartDocument],
     })
   return (
-    <div>
-      <div className="space-y-2">
-        <div className="flex gap-4">
-          <Image
-            src={item.image || ""}
-            width={75}
-            height={75}
-            alt={item.name}
-            objectFit="cover"
-          />
-          <div className="flex justify-between items-baseline flex-1 gap-2">
-            <span className="text-lg">{item.name}</span>
-            <span className="text-sm font-light">
-              {item.unitTotal.formatted}
-            </span>
-          </div>
-        </div>
-        <div className="flex gap-2">
-          <div className="flex-1 flex">
-            <div className="px-2 py-1 font-light border border-neutral-700 flex-1">
-              {item.quantity}
-            </div>
-          </div>
+    <div className="space-y-2">
+      <div className="flex gap-4">
+        <Image
+          src={item.image || ""}
+          width={75}
+          height={75}
+          alt={item.name}
+          objectFit="cover"
+        />
+        <div className="flex justify-between items-baseline flex-1 gap-2">
+          <span className="text-lg">{item.name}</span>
+          <span className="text-sm font-light">{item.unitTotal.formatted}</span>
         </div>
       </div>
       <div className="flex gap-2">
         <div className="flex-1 flex">
           <div className="px-2 py-1 font-light border border-neutral-700 flex-1">
-            {" "}
             {item.quantity}
           </div>
           <button
@@ -55,8 +43,8 @@ export function CartItem({ item, cartId }: { item: CartItem; cartId: string }) {
                 variables: { input: { id: item.id, cartId } },
               })
             }
-            disabled={decreaseCartItem}
-            className="p-1 font-light border border-neutral-700 hover:bg-black hover:text-white"
+            disabled={decreasingCartItem}
+            className="p-1 font-light border border-neutral-700  hover:bg-black hover:text-white"
           >
             <MinusIcon />
           </button>
@@ -66,8 +54,8 @@ export function CartItem({ item, cartId }: { item: CartItem; cartId: string }) {
                 variables: { input: { id: item.id, cartId } },
               })
             }
-            disabled={increaseCartItem}
-            className="p-1 font-light border border-neutral-700 hover:bg-black hover:text-white"
+            disabled={increasingCartItem}
+            className="p-1 font-light border border-neutral-700  hover:bg-black hover:text-white"
           >
             <PlusIcon />
           </button>
